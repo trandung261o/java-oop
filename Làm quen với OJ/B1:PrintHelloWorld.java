5872881a2fd3a899d7bde99439967dd81cb899b5
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-08-29 14:53:45 +0700

    Create B1:PrintHelloWorld.java

---
+++ Làm quen với OJ/B1:PrintHelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	System.out.println(x);
+	System.out.println("Hello World !");
+	System.out.println("28tech C++ programming !");
+    }
+}
