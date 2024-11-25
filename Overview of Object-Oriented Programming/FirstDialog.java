f21fcc4915ed121f135bcda337c943aaea680f11
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-11-25 16:44:59 +0700

    week1

---
+++ Overview of Object-Oriented Programming/FirstDialog.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class FirstDialog {
+	public static void main(String args[]) {
+		JOptionPane.showMessageDialog(null, "Xin chao ban!");
+		System.exit(0);
+	}
+}
---
+++ Overview of Object-Oriented Programming/HelloWorld.java
@@ -0,0 +1,7 @@
+//HelloWorld.java
+//Chuong trinh hien thi dong chu "Hello World"
+public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+	}
+}
